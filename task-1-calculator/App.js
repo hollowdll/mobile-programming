a1303ef3eb3324e57581fbac16ca45ab02f2c29e
hollowdll <pixerchat@gmail.com>
2023-08-28 12:40:59 +0300

    task 1 done

--- task-1-calculator/App.js
+++ task-1-calculator/App.js
@@ -1,10 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [number1, setNumber1] = useState("");
+  const [number2, setNumber2] = useState("");
+  const [result, setResult] = useState("");
+
+  const calculateSum = () => {
+    if (number1.length > 0 && number2.length > 0) {
+      setResult(parseInt(number1) + parseInt(number2));
+    }
+  }
+
+  const calculateDifference = () => {
+    if (number1.length > 0 && number2.length > 0) {
+      setResult(parseInt(number1) - parseInt(number2));
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Result: {result}</Text>
+      <TextInput
+          style={styles.input}
+          onChangeText={num => setNumber1(num)}
+          keyboardType="numeric"
+          placeholder={"Enter number 1"}
+          value={number1}
+      />
+      <TextInput
+          style={styles.input}
+          onChangeText={num => setNumber2(num)}
+          keyboardType="numeric"
+          placeholder={"Enter number 2"}
+          value={number2}
+      />
+      <View style={styles.buttonRow}>
+        <Button title="+" onPress={calculateSum} />
+        <Button title="-" onPress={calculateDifference} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +52,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    width: 200,
+    borderWidth: 1,
+    borderColor: 'black',
+  },
+  buttonRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  }
 });
