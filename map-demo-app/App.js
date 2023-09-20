0b7485cb3a6caf1e78c9d251d74b68fd8be898cb
hollowdll <pixerchat@gmail.com>
2023-09-20 10:45:01 +0300

    map demo app

---
+++ map-demo-app/App.js
@@ -0,0 +1,35 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 60.200692,
+          longitude: 24.934302,
+          latitudeDelta: 0.0322,
+          longitudeDelta: 0.0221,
+        }}
+      >
+        <Marker coordinate={{ latitude: 60.200712, longitude: 24.934392 }} title="Here" />
+      </MapView>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  }
+});
